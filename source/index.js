5d1bec16c7e43600a9c9bbf716e663b347488eec
a.chepugov <oleksii.chepugov@ria.com>
2018-07-02 13:46:49 +0300

    init

--- source/index.js
+++ source/index.js
@@ -1,222 +1,288 @@
-export default class DateLocal {
-	constructor(date, tz = 0) {
+const patternTimezone = /^(\+|-)?(\d{2}):?(\d{2})?$/;
+
+export const INVALID_ZONE_FORMAT = 'Invalid timezone zone format';
+
+export default class DateTz {
+	/**
+	 * Wrapper above js native Date object for using timezone time instead of local (for current machine)
+	 * @param {Date|Number|String} date
+	 * @param {String} zone - {@link zone}
+	 * @example
+	 * import DateTz from 'DateTz';
+	 * const date = new DateTz('2000-01-01T00:00:00.000Z', '+0300');
+	 * i.toISOString(); // '2000-01-01T00:00:00.000Z'
+	 */
+	constructor(date, zone = '+00') {
 		this._date = new Date(date);
-		this._tz = tz;
+		this.zone = zone;
 	}
 
-	_toTZ() {
+	/**
+	 * @param {String} value - zone in {@link https://rfc2.ru/5322.rfc/print#p3.3 rfc2822} format
+	 * @example
+	 * import DateTz from 'DateTz';
+	 * const i = new DateTz('2000-01-01T00:00:00.000Z');
+	 * i.zone // '+0000';
+	 * i.getHours(); // 0
+	 * i.getUTCHours(); // 0
+	 * i.zone = '+0300';
+	 * i.zone; // '+0300'
+	 * i.getHours(); // 3
+	 * i.getUTCHours(); // 0
+	 */
+	set zone(value) {
+		const result = patternTimezone.exec(value);
+		if (result) {
+			let [, sign = '+', h = '00', m = '00'] = result;
+			this._h = Number.parseInt(`${sign}${h}`);
+			this._m = Number.parseInt(`${sign}${m}`);
+			this._zone = `${sign}${h}${m}`;
+		} else {
+			throw new Error(INVALID_ZONE_FORMAT);
+		}
+	}
+
+	get zone() {
+		return this._zone;
+	}
+
+	get _shifted() {
 		const date = new Date(this._date);
-		date.setUTCHours(date.getUTCHours() + this._tz);
+		date.setUTCHours(date.getUTCHours() + this._h, date.getUTCMinutes() + this._m);
 		return date;
 	}
 
+	set _shifted(value) {
+		const date = value;
+		const timestamp = date.setUTCHours(date.getUTCHours() - this._h, date.getUTCMinutes() - this._m);
+		this._date = date;
+		return timestamp;
+	}
+
 	getDate() {
-		return this._toTZ().getUTCDate();
+		return this._shifted.getUTCDate();
 	}
 
 	getDay() {
-		return this._toTZ().getUTCDay();
+		return this._shifted.getUTCDay();
 	}
 
 	getFullYear() {
-		return this._date.getFullYear();
+		return this._shifted.getUTCFullYear();
 	}
 
 	getHours() {
-		return this._toTZ().getUTCHours();
+		return this._shifted.getUTCHours();
 	}
 
 	getMilliseconds() {
-		return this._date.getMilliseconds();
+		return this._shifted.getUTCMilliseconds();
 	}
 
 	getMinutes() {
-		return this._date.getMinutes();
+		return this._shifted.getUTCMinutes();
 	}
 
 	getMonth() {
-		return this._toTZ().getUTCMonth();
+		return this._shifted.getUTCMonth();
 	}
 
 	getSeconds() {
-		return this._date.getSeconds()
+		return this._shifted.getUTCSeconds();
 	}
 
 	getTime() {
-		return this._date.getTime()
+		return this._date.getTime();
 	}
 
 	getTimezoneOffset() {
-		return this._toTZ().getTimezoneOffset();
+		return -(this._h * 60 + this._m);
 	}
 
 	getUTCDate() {
-		return this._date.getUTCDate()
+		return this._date.getUTCDate();
 	}
 
 	getUTCDay() {
-		return this._date.getUTCDay()
+		return this._date.getUTCDay();
 	}
 
 	getUTCFullYear() {
-		return this._date.getUTCFullYear()
+		return this._date.getUTCFullYear();
 	}
 
 	getUTCHours() {
-		return this._date.getUTCHours()
+		return this._date.getUTCHours();
 	}
 
 	getUTCMilliseconds() {
-		return this._date.getUTCMilliseconds()
+		return this._date.getUTCMilliseconds();
 
 	}
 
 	getUTCMinutes() {
-		return this._date.getUTCMinutes()
+		return this._date.getUTCMinutes();
 	}
 
 	getUTCMonth() {
-		return this._date.getUTCMonth()
+		return this._date.getUTCMonth();
 	}
 
 	getUTCSeconds() {
-		return this._date.getUTCSeconds()
+		return this._date.getUTCSeconds();
 	}
 
-	getYear() {
-		return this._date.getYear()
-	}
-// @todo как
-	setDate(...args) {
-		return this._date.setDate(...args)
+	setDate() {
+		const _shifted = this._shifted;
+		_shifted.setUTCDate.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setFullYear(...args) {
-		return this._date.setFullYear(...args)
+	setFullYear() {
+		const _shifted = this._shifted;
+		_shifted.setUTCFullYear.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setHours(...args) {
-		return this._date.setHours(...args)
+	setHours() {
+		const _shifted = this._shifted;
+		_shifted.setUTCHours.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setMilliseconds(...args) {
-		return this._date.setMilliseconds(...args)
+	setMilliseconds() {
+		const _shifted = this._shifted;
+		_shifted.setUTCMilliseconds.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setMinutes(...args) {
-		return this._date.setMinutes(...args)
+	setMinutes() {
+		const _shifted = this._shifted;
+		_shifted.setUTCMinutes.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setMonth(...args) {
-		return this._date.setMonth(...args)
+	setMonth() {
+		const _shifted = this._shifted;
+		_shifted.setUTCMonth.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setSeconds(...args) {
-		return this._date.setSeconds(...args)
+	setSeconds() {
+		const _shifted = this._shifted;
+		_shifted.setUTCSeconds.apply(_shifted, arguments);
+		this._shifted = _shifted;
 	}
 
-	setTime(...args) {
-		return this._date.setTime(...args)
+	setTime() {
+		return this._date.setTime.apply(this._date, arguments);
 	}
 
-	setUTCDate(...args) {
-		return this._date.setUTCDate(...args)
+	setUTCDate() {
+		return this._date.setUTCDate.apply(this._date, arguments);
 	}
 
-	setUTCFullYear(...args) {
-		return this._date.setUTCFullYear(...args)
+	setUTCFullYear() {
+		return this._date.setUTCFullYear.apply(this._date, arguments);
 	}
 
-	setUTCHours(...args) {
-		return this._date.setUTCHours(...args)
+	setUTCHours() {
+		return this._date.setUTCHours.apply(this._date, arguments);
 	}
 
-	setUTCMilliseconds(...args) {
-		return this._date.setUTCMilliseconds(...args)
+	setUTCMilliseconds() {
+		return this._date.setUTCMilliseconds.apply(this._date, arguments);
 	}
 
-	setUTCMinutes(...args) {
-		return this._date.setUTCMinutes(...args)
+	setUTCMinutes() {
+		return this._date.setUTCMinutes.apply(this._date, arguments);
 	}
 
-	setUTCMonth(...args) {
-		return this._date.setUTCMonth(...args)
+	setUTCMonth() {
+		return this._date.setUTCMonth.apply(this._date, arguments);
 	}
 
-	setUTCSeconds(...args) {
-		return this._date.setUTCSeconds(...args)
-	}
-
-	setYear(...args) {
-		return this._date.setYear(...args)
+	setUTCSeconds() {
+		return this._date.setUTCSeconds.apply(this._date, arguments);
 	}
 
 	toDateString() {
-		return this._date.toDateString()
-	}
-
-	toGMTString() {
-		return this._date.toGMTString()
+		return this._date.toDateString.apply(this._date, arguments);
 	}
 
 	toISOString() {
-		return this._date.toISOString()
+		return this._date.toISOString.apply(this._date, arguments);
 	}
 
 	toJSON() {
-		return this._date.toJSON()
+		return this._date.toJSON.apply(this._date, arguments);
 	}
 
 	toLocaleDateString() {
-		return this._date.toLocaleDateString()
-	}
-
-	toLocaleFormat() {
-		return this._date.toLocaleFormat()
+		return this._date.toLocaleDateString.apply(this._date, arguments);
 	}
 
 	toLocaleString() {
-		return this._date.toLocaleString()
+		return this._date.toLocaleString.apply(this._date, arguments);
 	}
 
 	toLocaleTimeString() {
-		return this._date.toLocaleTimeString()
-	}
-
-	toSource() {
-		return this._date.toSource()
+		return this._date.toLocaleTimeString.apply(this._date, arguments);
 	}
 
 	toString() {
-		return this._date.toString()
+		const _shifted = this._shifted;
+		const options = {
+			weekday: 'short',
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZone: 'UTC',
+			hour12: false
+		};
+
+		let string = _shifted.toLocaleString('en-US', options);
+		return `${string} GMT${this.zone}`;
 	}
 
 	toTimeString() {
-		return this._date.toTimeString()
+		const _shifted = this._shifted;
+		const options = {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZone: 'UTC',
+			hour12: false
+		};
+		let string = _shifted.toLocaleTimeString('en-US', options);
+		return `${string} GTM${this.zone}`;
 	}
 
 	toUTCString() {
-		return this._date.toUTCString()
+		return this._date.toUTCString.apply(this._date, arguments);
 	}
 
 	valueOf() {
-		return this._date.valueOf()
+		return this._date.valueOf.apply(this._date, arguments);
 	}
 
 	/**
-	 npm install --save DateLocal
+	 npm install --save DateTz
 	 @name Installation
 	 */
 }
 
-DateLocal.UTC = function (...args) {
-	return new DateLocal(Date.UTC(...args));
+DateTz.UTC = function (...args) {
+	return new DateTz(Date.UTC(...args));
 };
 
-DateLocal.now = function (...args) {
-	return new DateLocal(Date.now(...args));
+DateTz.now = function () {
+	return new DateTz(Date.now());
 };
 
-DateLocal.parse = function (...args) {
-	return new DateLocal(Date.parse(...args));
+DateTz.parse = function (...args) {
+	return new DateTz(Date.parse(...args));
 };
--- source/index.test.js
+++ source/index.test.js
@@ -4,38 +4,311 @@ require('babel-polyfill');
 const expect = require('chai').expect;
 
 const Class = require('./index').default;
+const {INVALID_ZONE_FORMAT} = require('./index');
 
 describe('initialization', function () {
 
+	it('Date', async function () {
+		let i = new Class(new Date('2000-01-01T00:00:00.000Z'), '+03');
+		expect(typeof i).to.equal('object');
+	});
+
 	it('Number', async function () {
-		let i = new Class(946684800000, 12);
+		let i = new Class(946684800000, '12');
+		expect(typeof i).to.equal('object');
 	});
 
 	it('String', async function () {
-		let i = new Class('2000-01-01T00:00:00.000Z', -1);
+		let i = new Class('2000-01-01T00:00:00.000Z', '-01');
+		expect(typeof i).to.equal('object');
 	});
 
-	it('Date', async function () {
-		let i = new Class(new Date('2000-01-01T00:00:00.000Z', 3));
+	describe(INVALID_ZONE_FORMAT, function () {
+
+		it('0', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', '')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+		it('1', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', '1')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+		it('2', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', 'd1')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+		it('3', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', '603')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+		it('4', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', '6d03')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+		it('5', async function () {
+			expect(() => new Class(new Date('2000-01-01T00:00:00.000Z', '+12345')).to.throw(INVALID_ZONE_FORMAT));
+		});
+
+	});
+});
+
+describe('methods', function () {
+	it('setMilliseconds', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z', '06');
+		i.setMilliseconds(15);
+		expect(i.getMilliseconds()).to.equal(15);
+		expect(i.getUTCMilliseconds()).to.equal(15);
+	});
+
+	it('setMilliseconds. negative zone', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z', '-06');
+		i.setMilliseconds(15);
+		expect(i.getMilliseconds()).to.equal(15);
+		expect(i.getUTCMilliseconds()).to.equal(15);
+	});
+
+	it('setSeconds', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z', '06');
+		i.setSeconds(15);
+		expect(i.getSeconds()).to.equal(15);
+		expect(i.getUTCSeconds()).to.equal(15);
+	});
+
+	it('setSeconds. negative zone', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z', '-06');
+		i.setSeconds(15);
+		expect(i.getSeconds()).to.equal(15);
+		expect(i.getUTCSeconds()).to.equal(15);
+	});
+
+	it('setMinutes. negative zone', async function () {
+		const i = new Class('2000-01-01T06:00:00.000Z', '-0603');
+		i.setMinutes(15);
+		expect(i.getMinutes()).to.equal(15);
+		expect(i.getUTCMinutes()).to.equal(18);
+	});
+
+	it('setHours', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z', '06');
+		i.setHours(12);
+		expect(i.getHours()).to.equal(12);
+		expect(i.getUTCHours()).to.equal(6);
+	});
+
+	it('setHours. negative zone', async function () {
+		const i = new Class('2000-01-01T06:00:00.000Z', '-06');
+		const q = i.setHours(0);
+		expect(i.getHours()).to.equal(0);
+		expect(i.getUTCHours()).to.equal(6);
+	});
+
+	it('setDate', async function () {
+		const i = new Class('2000-01-10T00:00:00.000+06:00', '06');
+		i.setDate(12);
+		expect(i.getDate()).to.equal(12);
+		expect(i.getUTCDate()).to.equal(11);
+	});
+
+	it('setDate. negative zone', async function () {
+		const i = new Class('2000-01-10T20:00:00.000-06:00', '-06');
+		i.setDate(12);
+		expect(i.getDate()).to.equal(12);
+		expect(i.getUTCDate()).to.equal(13);
+	});
+
+	it('getDay', async function () {
+		const i = new Class('2000-01-10T00:00:00.000+06:00', '06');
+		i.setDate(12);
+		expect(i.getDay()).to.equal(3);
+		expect(i.getUTCDay()).to.equal(2);
+	});
+
+	it('getDay. negative zone', async function () {
+		const i = new Class('2000-01-10T20:00:00.000-06:00', '-06');
+		i.setDate(12);
+		expect(i.getDay()).to.equal(3);
+		expect(i.getUTCDay()).to.equal(4);
+	});
+
+	it('setMonth', async function () {
+		const i = new Class('2000-01-01T00:00:00.000+06:00', '06');
+		i.setMonth(5);
+		expect(i.getMonth()).to.equal(5);
+		expect(i.getUTCMonth()).to.equal(4);
+	});
+
+	it('setMonth. negative zone', async function () {
+		const i = new Class('2000-01-30T20:00:00.000-06:00', '-06');
+		i.setMonth(5);
+		expect(i.getMonth()).to.equal(5);
+		expect(i.getUTCMonth()).to.equal(6);
+	});
+
+	it('setFullYear', async function () {
+		const i = new Class('2000-01-01T00:00:00.000+06:00', '06');
+		i.setFullYear(2002);
+		expect(i.getFullYear()).to.equal(2002);
+		expect(i.getUTCFullYear()).to.equal(2001);
+	});
+
+	it('setFullYear. negative zone', async function () {
+		const i = new Class('2000-12-31T20:00:00.000-06:00', '-06');
+		i.setFullYear(2001);
+		expect(i.getFullYear()).to.equal(2001);
+		expect(i.getUTCFullYear()).to.equal(2002);
+	});
+
+	it('setTime', async function () {
+		const i = new Class('2000-01-01T00:00:00.000+06:00', '06');
+		i.setTime(946684800000);
+		expect(i.getTime()).to.equal(946684800000);
+	});
+
+	it('getTimezoneOffset', async function () {
+		const i = new Class('2000-01-01T00:00:00.000+06:00', '1206');
+		i.setMonth(5);
+		expect(i.getTimezoneOffset()).to.equal(-726);
+	});
+
+	it('getTimezoneOffset. negative zone', async function () {
+		const i = new Class('2000-01-30T20:00:00.000-06:00', '-0507');
+		i.setMonth(5);
+		expect(i.getTimezoneOffset()).to.equal(307);
+	});
+
+	it('toDateString', async function () {
+		const i = new Class('2000-01-30T20:00:00.000-06:00', '-0507');
+		i.setDate(31);
+		expect(i.toDateString()).to.equal('Tue Feb 01 2000');
+	});
+
+	it('toISOString', async function () {
+		const i = new Class('2000-01-30T20:00:00.000-06:00', '-0507');
+		i.setDate(31);
+		expect(i.toISOString()).to.equal('2000-02-01T02:00:00.000Z');
+	});
+
+	it('toJSON', async function () {
+		const i = new Class('2000-01-30T20:00:00.000-06:00', '-0507');
+		i.setDate(31);
+		expect(i.toJSON()).to.equal('2000-02-01T02:00:00.000Z');
+	});
+
+	it('toLocaleDateString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00');
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			timeZone: 'UTC'
+		};
+		expect(i.toLocaleDateString('en-US', options)).to.equal('Tuesday, February 1, 2000, 2:00 AM');
+	});
+
+	it('toLocaleString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00');
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			timeZone: 'UTC'
+		};
+		expect(i.toLocaleString('en-US', options)).to.equal('Tuesday, February 1, 2000, 2:00 AM');
+	});
+
+	it('toLocaleTimeString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00');
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			timeZone: 'UTC'
+		};
+		expect(i.toLocaleTimeString('en-US', options)).to.equal('Tuesday, February 1, 2000, 2:00 AM');
+	});
+
+	it('toString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00', '-06');
+		expect(i.toString()).to.equal('Mon, Jan 31, 2000, 20:00:00 GMT-0600');
+	});
+
+	it('toTimeString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00', '-06');
+		expect(i.toTimeString()).to.equal('20:00:00 GTM-0600');
+	});
+
+	it('toUTCString', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00');
+		expect(i.toUTCString()).to.equal('Tue, 01 Feb 2000 02:00:00 GMT');
+	});
+
+	it('valueOf', async function () {
+		const i = new Class('2000-01-31T20:00:00.000-06:00');
+		expect(i.valueOf()).to.equal(949370400000);
 	});
 });
 
 describe('static methods', function () {
 
-	it('now', async function () {
-		const i = Class.now();
-		console.log('DEBUG:index.test.js():29 =>', i);
+	it('UTC', async function () {
+		const i = Class.UTC(2000, 11, 31, 23, 2);
+		expect(i.getUTCFullYear()).to.equal(2000);
+		expect(i.getUTCMonth()).to.equal(11);
+		expect(i.getUTCDate()).to.equal(31);
+		expect(i.getUTCHours()).to.equal(23);
+		expect(i.getUTCMinutes()).to.equal(2);
 	});
 
-});
+	it('now', async function () {
+		const i = Class.now();
+		const nowDate = new Date();
 
+		expect(i.getUTCFullYear()).to.equal(nowDate.getUTCFullYear());
+		expect(i.getUTCMonth()).to.equal(nowDate.getUTCMonth());
+		expect(i.getUTCDate()).to.equal(nowDate.getUTCDate());
+		expect(i.getUTCHours()).to.equal(nowDate.getUTCHours());
+		expect(i.getUTCMinutes()).to.equal(nowDate.getUTCMinutes());
+	});
 
-describe('methods', function () {
+	it('parse', async function () {
+		const i = Class.parse('Aug 9, 1995');
+		expect(i.getUTCFullYear()).to.equal(1995);
+		expect(i.getUTCMonth()).to.equal(7);
+		expect(i.getUTCDate()).to.equal(8);
+	});
 
-	it('getHours', async function () {
-		const i = new Class('2000-01-01T18:00:00.000Z', 12);
-		expect(i.getHours()).to.equal(6);
-		expect(i.getDate()).to.equal(2);
+	it('zone change', async function () {
+		const i = new Class('2000-01-01T00:00:00.000Z');
+		expect(i.zone).to.equal('+0000');
+		expect(i.getHours()).to.equal(0);
+		expect(i.getUTCHours()).to.equal(0);
+
+		i.zone = '+0300';
+		expect(i.zone).to.equal('+0300');
+		expect(i.getHours()).to.equal(3);
+		expect(i.getUTCHours()).to.equal(0);
+
+		i.setHours(5);
+		expect(i.getHours(5)).to.equal(5);
+		expect(i.getUTCHours()).to.equal(2);
+
+		i.setUTCHours(5);
+		expect(i.getHours(5)).to.equal(8);
+		expect(i.getUTCHours()).to.equal(5);
+
+		i.zone = '-0200';
+		expect(i.zone).to.equal('-0200');
+		expect(i.getHours()).to.equal(3);
+		expect(i.getUTCHours()).to.equal(5);
 	});
 
 });
